394f6a289cab1de2c88b71bb4550e2f5c2309f6a
Polina Lappo <polina.lappo@soft-werke.com>
2018-12-29 12:32:04 +0300

    task 1

--- lesson-3.js
+++ lesson-3.js
@@ -1,7 +1,19 @@
 "use strict";
 //task 1
-var a = 0;
-while(a<=100){
-    console.log(a);
-    a++;
+var n = 0;
+var i, j;
+while (n <= 100) {
+    i = 2;
+    j = 0;
+    while ((i * i <= n) && (j != 1)) {
+        if (n % i == 0) {
+            j = 1;
+        }
+        i++;
+    }
+
+    if ((j == 0) && (n!=0) ){
+        console.log(n);
+    }
+    n++;
 }
