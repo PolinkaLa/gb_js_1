4ff0f921204a1da250500c22b94dbcfd8895b39d
Polina Lappo <polina.lappo@soft-werke.com>
2019-01-13 20:52:48 +0300

    add render goods

---
+++ lesson-6/task-2/goods.js
@@ -0,0 +1,20 @@
+var goods = [
+    {
+        id: "good_1",
+        name: "Конфеты",
+        img: "img/good_1.jpg",
+        cost: 100
+    },
+    {
+        id: "good_2",
+        name: "Ножницы",
+        img: "img/good_2.jpg",
+        cost: 200
+    },
+    {
+        id: "good_3",
+        name: "Краски",
+        img: "img/good_3.jpg",
+        cost: 300
+    }
+]
